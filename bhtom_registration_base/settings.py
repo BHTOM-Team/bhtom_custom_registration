bb0258252adfeb877c399cfa41247180cba0a6a7
Maja Jabłońska <majajjablonska@gmail.com>
2022-05-04 22:07:35 +0200

    Correct bhtom modules paths in bhtom_registration settings.py

--- bhtom_registration_base/settings.py
+++ bhtom_registration_base/settings.py
@@ -225,15 +225,15 @@ DATA_PRODUCT_TYPES = {
 }
 
 DATA_PROCESSORS = {
-    'photometry': 'bhtom_dataproducts.processors.photometry_processor.PhotometryProcessor',
-    'spectroscopy': 'bhtom_dataproducts.processors.spectroscopy_processor.SpectroscopyProcessor',
+    'photometry': 'bhtom_base.bhtom_dataproducts.processors.photometry_processor.PhotometryProcessor',
+    'spectroscopy': 'bhtom_base.bhtom_dataproducts.processors.spectroscopy_processor.SpectroscopyProcessor',
 }
 
 TOM_FACILITY_CLASSES = [
-    'bhtom_observations.facilities.lco.LCOFacility',
-    'bhtom_observations.facilities.gemini.GEMFacility',
-    'bhtom_observations.facilities.soar.SOARFacility',
-    'bhtom_observations.facilities.lt.LTFacility'
+    'bhtom_base.bhtom_observations.facilities.lco.LCOFacility',
+    'bhtom_base.bhtom_observations.facilities.gemini.GEMFacility',
+    'bhtom_base.bhtom_observations.facilities.soar.SOARFacility',
+    'bhtom_base.bhtom_observations.facilities.lt.LTFacility'
 ]
 
 TOM_ALERT_CLASSES = [
@@ -285,9 +285,9 @@ TARGET_PERMISSIONS_ONLY = True
 OPEN_URLS = []
 
 HOOKS = {
-    'target_post_save': 'bhtom_common.hooks.target_post_save',
-    'observation_change_state': 'bhtom_common.hooks.observation_change_state',
-    'data_product_post_upload': 'bhtom_dataproducts.hooks.data_product_post_upload'
+    'target_post_save': 'bhtom_base.bhtom_common.hooks.target_post_save',
+    'observation_change_state': 'bhtom_base.bhtom_common.hooks.observation_change_state',
+    'data_product_post_upload': 'bhtom_base.bhtom_dataproducts.hooks.data_product_post_upload'
 }
 
 AUTO_THUMBNAILS = False
