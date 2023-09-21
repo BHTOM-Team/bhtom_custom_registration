f235fcc019fccf84ea0561a2b18a5758fee0c235
arturkrawczyk <akrawczyk@akond.com>
2023-09-21 12:57:43 +0200

    add cron

--- bhtom_registration/registration_flows/approval_required/views.py
+++ bhtom_registration/registration_flows/approval_required/views.py
@@ -1,20 +1,24 @@
-import logging
+import os
 from smtplib import SMTPException
 
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.models import User, Group
 from django.contrib.sites.models import Site
-from django.core.mail import mail_managers, send_mail
+from django.core.mail import send_mail
 from django.views.generic.edit import CreateView, UpdateView
 from django.shortcuts import redirect, reverse
 from django.urls import reverse_lazy
+from dotenv import dotenv_values
 
+from bhtom2.settings import BASE_DIR
+from bhtom2.utils.bhtom_logger import BHTOMLogger
 from bhtom_base.bhtom_common.mixins import SuperuserRequiredMixin
 from bhtom_custom_registration.bhtom_registration.registration_flows.approval_required.forms import ApproveUserForm
 from bhtom_custom_registration.bhtom_registration.registration_flows.approval_required.forms import RegistrationApprovalForm
 
-logger = logging.getLogger(__name__)
+logger: BHTOMLogger = BHTOMLogger(__name__, '[bhtom_registration]')
+secret = dotenv_values(os.path.join(BASE_DIR, 'bhtom2/.bhtom.env'))
 
 
 # TODO: Add post-approval hooks that actually handle the sending of email
@@ -38,14 +42,12 @@ class ApprovalRegistrationView(CreateView):
 
         if settings.TOM_REGISTRATION.get('SEND_APPROVAL_EMAILS'):
             try:
-                current_domain = Site.objects.get_current().domain
-                link_to_user_list = f'https://{current_domain}{reverse("user-list")}'
-                mail_managers(
-                    f'Registration Request from {self.object.first_name} {self.object.last_name}',
-                    f'{self.object.first_name} {self.object.last_name} has requested to register in your TOM. Please '
-                    f'approve or delete this user <a href="{link_to_user_list}">here</a>.',
-                    fail_silently=False
-                )
+                send_mail(secret.get("EMAILTET_ACTIVATEUSER_TITLE", "Account activation"),
+                          secret.get('EMAILTEXT_REGISTEUSER'),
+                          settings.EMAIL_HOST_USER, [user_email.email], fail_silently=False)
+
+                logger.info('Create User' + instance.obsName + ', Send mail: ' + user_email.email)
+
             except SMTPException as smtpe:
                 logger.error(f'Unable to send email: {smtpe}')
 
